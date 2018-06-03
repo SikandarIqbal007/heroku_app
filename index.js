f9625d32541fb56367f6d6e7afdad33ca315b4a8
sikandar <sikndar.iqbal@gmail.com>
2018-06-03 04:44:41 -0700

    'nice'

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
-const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.send('SIKANDAR IS A GOOD BAD BOY'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+// express()
+ 
+//   .get('/', (req, res) => res.send('SIKANDAR IS A GOOD BAD BOY'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.get('/', function(req,res){
+res.send('I want a pizza right now!');
+});
+
+app.listen(PORT);
